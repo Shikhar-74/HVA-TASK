5fca48dacee0c1438a2ec0f2b8e974773771d422
Shikhar-Shuklaa <shikhar23@navgurukul.org>
2024-08-04 21:58:42 +0530

    javascript file

---
+++ javascript/Function/Greet_function.js
@@ -0,0 +1,42 @@
+// function greet(username,profession){
+//    console.log(`my name is ${username}`);
+//    console.log(`i am ${profession} of navgurukul`);
+// }
+// greet('shikhar','software developer')
+
+
+
+// a = Greet function
+// function greet(name){
+//    return `Hello ${name}`
+// }
+// console.log(greet('shikhar'));
+
+
+// b greetDefault function
+// function greetDefault(guest='guest'){
+//    return `Hello ${guest}`
+// }
+// console.log(greetDefault());
+
+
+
+// c rewrite greet as a function expression
+// let greet = function(name){
+//     return `Hello ${name}`
+// }
+// console.log(greet('shikhar'));
+
+
+// d rewrite greet as arrow function
+// let greet = (name)=>{
+//     return `Hello ${name}`
+// }
+// console.log(greet('shikhar'));
+
+
+
+
+function sum(a,b,cb){
+    return cb(a+b)
+}
